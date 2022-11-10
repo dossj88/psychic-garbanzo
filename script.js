985085f819543010601a2bf763d8c238b389f38c
jasondoss <jason.j.doss@gmail.com>
2022-11-10 09:48:24 -0800

    added funtion for password criteria and confirm messages for what the user wants as characters in their password

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
+// Array for uppercase letters
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+// Array for lowercase letters
+
+var lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+"w","x","y","z"];
+// Added parameters for length, and number of characters using if statements
+
 // Array of special characters.
 
-var 
-specialCharacters = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "+",
-  "=",
-  "(",
-  ")",
-  "~",
-  "`",
-  "<",
-  ">",
-  ":",
-  ";",
-  "{",
-  "}",
-  "[",
-  "]",
-
-];
+var special = ["!","@","#","$","%","^","&","*","+","=","(",")","~","`","<",">",":",";","{","}","[","]"];
 
 // Array for numbers
 
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8,", "9"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8,", "9"];
 
-// Array for uppercase letters
+// made password criteria
+function passwordCriteria() {
+  var length = parseInt(
+    prompt("How many characters would you like?"),
+     8
+      );
 
-var upperCaseCasedCharacter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+      if (Number.isNan(length)) {
+        alert("Password length must be a number");
+        return null;
+        
+      }
+
+      if (pass.length < 8) {
+        alert("Password must be at least 8 characters long");
+        return null;
+        
+      }
+
+      if (pass.length > 128) {
+        alert("Password length must be no more than 128 characters long");
+        return null;
+        
+      }
+//  made confirm messages
+      var hasSpecial = confirm(
+        "Click OK to confirm special characters. "
+      );
+
+      var hasNumeric = confirm(
+        "Click OK to confirm numbers."
+      );
+      
+      var hasLowerCase = confirm(
+        "Click OK to confirm lower case letters."
+      );
+
+      var hasUpperCase = confirm(
+        "Click OK to confirm upper case letters."
+      );
 
 
 
-var lowerCasedCharacters = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
-"w","x","y","z"];
 
-
+  
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
