61f9c04cf4929bf55768a091383741ddff7d7af3
jasondoss <jason.j.doss@gmail.com>
2022-11-10 23:30:19 -0800

    added if criteria and conditional statements to script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 
 // Array for lowercase letters
 
-var lowerCase = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
 "w","x","y","z"];
 // Added parameters for length, and number of characters using if statements
 
@@ -20,22 +20,21 @@ var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8,", "9"];
 function passwordCriteria() {
   var length = parseInt(
     prompt("How many characters would you like?"),
-     8
-      );
-
-      if (Number.isNan(length)) {
+     10);
+  
+      if (Number.isNaN(length)) {
         alert("Password length must be a number");
         return null;
         
       }
 
-      if (pass.length < 8) {
+      if (length < 8) {
         alert("Password must be at least 8 characters long");
         return null;
         
       }
 
-      if (pass.length > 128) {
+      if (length > 128) {
         alert("Password length must be no more than 128 characters long");
         return null;
         
@@ -57,12 +56,78 @@ function passwordCriteria() {
         "Click OK to confirm upper case letters."
       );
 
-
-
-
+if (
+  hasLowerCase === false &&
+  hasUpperCase === false &&
+  hasNumeric === false &&
+  hasSpecial === false 
+) {
   
+  alert("Must have at lease one character type");
+  return null;
+
 }
 
+var passwordCriteria = {
+  length: length,
+  hasLowerCase: hasLowerCase,
+  hasUpperCase: hasUpperCase,
+  hasNumeric: hasNumeric,
+  hasSpecial: hasSpecial,
+};
+
+return passwordCriteria;
+}
+
+function getRandom(arr) {
+ var index = Math.floor(Math.random() * arr.length);
+  var element = arr[index];
+  return element;
+  }
+
+function generatePassword() {
+  var options = passwordCriteria();
+
+  var result = [];
+
+  var possible = [];
+
+  var actual = [];
+
+  if (!options) return null;
+
+  if (options.hasSpecial) {
+    possible = possible.concat(special);
+    actual.push(getRandom(special));
+    }
+  
+    if (options.hasNumeric) {
+      possible = possible.concat(numeric);
+      actual.push(getRandom(numeric));
+    }
+
+    if (options.hasLowerCase) {
+      possible = possible.concat(lowerCase);
+      actual.push(getRandom(lowerCase));
+    }
+
+    if (options.hasUpperCase) {
+      possible = possible.concat(upperCase);
+      actual.push(getRandom(upperCase));
+    }
+
+ for (var i = 0; i < options.length; i++) {
+    var possible = getRandom(possible);
+
+    result.push(possible);
+ }
+
+     for (var i = 0; i < actual.length; i++) {
+    result[i] = actual[i];
+  }
+  
+  return result.join("");  
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
